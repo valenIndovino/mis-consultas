72a4b472f2e4811003888905fa9719c5ceed5acd
Valentino Indovino <valetero30@gmail.com>
2022-06-15 22:32:31 -0300

    Tratamos el filtrado de los turnos

--- src/views/TurnosView.vue
+++ src/views/TurnosView.vue
@@ -2,7 +2,30 @@
   <div class="about">
     <AppHeader :admin="admin" />
     <router-view />
-    <h1>Turnos</h1>
+    <h1 class="mt-2">Turnos</h1>
+    <label>Filtrar por:</label>
+    <select
+      class="form-select m-2"
+      id="select"
+      v-on:change="filtrar()"
+      v-model="specialty"
+    >
+      <option>Sin filtro</option>
+      <option v-for="especialidad in especialidades" :key="especialidad.id">
+        {{ especialidad.nombre }}
+      </option>
+    </select>
+    <select
+      class="form-select m-2"
+      id="select"
+      v-on:change="filtrar()"
+      v-model="state"
+    >
+      <option>Sin filtro</option>
+      <option v-for="estado in estados" :key="estado.id">
+        {{ estado.nombre }}
+      </option>
+    </select>
     <table class="table table-hover table-primary">
       <thead>
         <tr>
@@ -42,6 +65,10 @@ export default {
     return {
       turnos: [],
       admin: store.getters.getUser.admin,
+      especialidades: store.getters.getEspecialidades,
+      estados: store.getters.getEstados,
+      specialty: null,
+      state: null,
     };
   },
   components: {
@@ -72,6 +99,43 @@ export default {
     },
     getEstadoById(id) {
       return store.getters.getEstadoById(id);
+    },
+    filtrar() {
+      let idEstado = this.estados.find((item) => item.nombre == this.state);
+      let idEspecialidad = this.especialidades.find(
+        (item) => item.nombre == this.specialty
+      );
+      console.log("ESTADO", idEstado);
+      console.log("ESPECIALIADD", idEspecialidad);
+      let url = "https://628c24e1a3fd714fd02d5a68.mockapi.io/Turnos";
+      if (idEstado != undefined) {
+        url = "https://628c24e1a3fd714fd02d5a68.mockapi.io/Turnos";
+        url = url + "/?estado=" + idEstado.id;
+      }
+      if (idEspecialidad != undefined) {
+        url = "https://628c24e1a3fd714fd02d5a68.mockapi.io/Turnos";
+        url = url + "/?especialidad=" + idEspecialidad.id;
+      }
+      if (idEstado != undefined && idEspecialidad != undefined) {
+        url = "https://628c24e1a3fd714fd02d5a68.mockapi.io/Turnos";
+        url =
+          url +
+          "/?especialidad=" +
+          idEspecialidad.id +
+          "/?estado=" +
+          idEstado.id;
+      }
+      this.turnos = [];
+      fetch(url, {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          data.map((item) => {
+            this.turnos.push(item);
+          });
+        })
+        .catch((err) => console.log(err.message));
     },
   },
 };
